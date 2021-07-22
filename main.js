bc339196f1cdeef4a3b030ad882797d8fe35a593
Rayan20107 <75358640+Rayan20107@users.noreply.github.com>
2021-07-22 15:38:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function preload()
+{
+  clownose=loadImage('https://i.postimg.cc/k4JV27Cv/Clown-Nose-PNG-Image.png');
+}
+
+function modelLoaded()
+{
+    console.log("Posenet model initialized");
+}
+
+function gotposes(results)
+{
+    if (results.length>0) {
+        console.log(results);
+    }
+}
+
+function setup()
+{
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    webcam=createCapture(VIDEO);
+    webcam.size(300, 300);
+    webcam.hide();
+    posenet=ml5.poseNet(webcam, modelLoaded);
+    posenet.on('pose', gotposes);
+}
+
+function draw()
+{
+   image(webcam, 0, 0, 300, 300);
+}
+
+function take_snapshot()
+{
+    save('Whatsapp-MoustacheNose-Filter.png');
+}
